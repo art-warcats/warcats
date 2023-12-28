1820e6e1d8a05a59b2207630970bb47b8daf061b
Shawn Shroyer <shroysha@gmail.com>
2023-12-28 16:44:11 -0500

    adding warcats code

---
+++ apps/api/src/app/app.module.ts
@@ -0,0 +1,45 @@
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+
+import {StarrybotController} from '../starrybot/controller/starrybot.controller';
+import {WarCatsController} from '../warcats/controller/warcats.controller';
+import {WarCatsMatchingService} from '../warcats/service/warcats.matching.service';
+import {WarCatsGateway} from '../warcats/gateway/warcats.gateway';
+import {makeRedisProvider} from '../warcats/redis/warcats.redis';
+import {MongooseModule} from '@nestjs/mongoose';
+import {ThrottlerModule} from '@nestjs/throttler';
+import {throwErr} from '../helpers/throwErr';
+import {gameSchema} from '../warcats/schema/game.schema';
+import {WarCatsGameService} from '../warcats/service/warcats.game.service';
+import { AppController } from './controller/app.controller';
+import { ScheduleModule } from '@nestjs/schedule';
+
+@Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.local', '.env'],
+    }),
+    ThrottlerModule.forRoot({limit: 1000, ttl: 69}),
+    MongooseModule.forRoot(
+      process.env.MONGO_URL ?? throwErr('MONGO_URL not defined'),
+    ),
+    MongooseModule.forFeature([
+      {
+        name: 'Game',
+        schema: gameSchema,
+        collection: 'games',
+      },
+    ]),
+  ],
+  controllers: [AppController, StarrybotController, WarCatsController],
+  providers: [
+    makeRedisProvider('REDIS'),
+    WarCatsMatchingService,
+    WarCatsGameService,
+    WarCatsGateway,
+  
+  ],
+})
+export class AppModule {}
